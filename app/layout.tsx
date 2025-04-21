d3271646183697ffaa7c240a7f21f66b9515bccc
Chloe-SD <chloe.nibali@edu.sait.ca>
2025-04-21 14:38:21 -0600

    added opengraph specs to customize link shareing view

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,30 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Chloe Nibali - Developer",
   description: "Portfolio of Chloe Nibali",
+  openGraph: {
+    title: 'Chloe - Web & Mobile Developer',
+    description: 'Portfolio of Chloe, web and mobile developer bringing innovative ideas to life.',
+    url: 'https://codebychloe.com',
+    siteName: 'Chloe\'s Portfolio',
+    images: [
+      {
+        url: 'https://codebychloe.com/socialShare.png', 
+        width: 1200,
+        height: 630,
+        alt: 'Chloe\'s Portfolio',
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Chloe - Full-Stack Developer',
+    description: 'Portfolio of Chloe, Full-Stack Software Developer.',
+    images: ['https://codebychloe.com/socialShare.png'],
+  },
 };
+
 
 export default function RootLayout({
   children,
