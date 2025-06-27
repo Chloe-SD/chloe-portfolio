1f250dcb0f378e8b890604dac3da5d6847ba665b
Chloe-SD <chloe.nibali@edu.sait.ca>
2025-06-27 15:49:41 -0600

    resized bg image for faster loading + added default bg color for less jarring transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          as="image"
+          href="/cmore1.jpg"
+        />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
